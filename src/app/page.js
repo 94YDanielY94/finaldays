9484584636d7838cf1223eaac374d4cf10a9257b
Daniel <146764509+94YDanielY94@users.noreply.github.com>
2025-07-03 13:10:20 +0300

    remote

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,13 @@
 
 export default function Home() {
   return (
-    <div></div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 ">
+      <div className="rounded-lg shadow-lg p-8 bg-white w-1/2">
+        <h1 className="text-3xl font-bold mb-4 text-center">Ethiopian Secondary School Leaving Certificate Examination(ESSLCE),Educational Assessment and Examinations Service(EAES)</h1>
+        <div>
+          <input ></input>
+        </div>
+      </div>
+    </div>
   );
 }
